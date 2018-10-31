8a7c88e24698623a5cfeace90dc6a819e16bc10d
felixmaomao <swfightforyl@163.com>
2018-10-31 17:14:10 +0800

    point effector

---
+++ unity-fundmental/Assets/BombManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombManager : MonoBehaviour
+{
+
+    public GameObject explosionPrefab;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("bomb");
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        GameObject explosion = Instantiate(explosionPrefab, gameObject.transform.position, Quaternion.identity);
+        //炸弹炸完了，自身得消失
+        gameObject.SetActive(false);
+        Destroy(explosion,0.3f);
+    }
+}
---
+++ unity-fundmental/Assets/CoinGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinGenerator : MonoBehaviour
+{
+
+    public GameObject coinPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            Instantiate(coinPrefab, this.gameObject.transform.position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
