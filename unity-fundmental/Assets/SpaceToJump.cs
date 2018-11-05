26bbbdb4a9df6c38fdee6b96c1461d272a522d5f
felixmaomao <swfightforyl@163.com>
2018-11-05 14:29:01 +0800

    platform effector surface effector

---
+++ unity-fundmental/Assets/SpaceToJump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceToJump : MonoBehaviour {
+
+
+     Rigidbody2D rigid;
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            AddUpperForce();
+        }	
+	}
+
+    void AddUpperForce()
+    {
+        rigid.AddForce(Vector2.up,ForceMode2D.Impulse);
+    }
+}
